2d86cc131470180cfae811d94d000b4c13d07821
Julio C. Perez-Baez <perezbjc@gmail.com>
2020-11-22 18:16:04 -0500

    Update generate-ssl-certs.sh

--- scripts/generate-ssl-certs.sh
+++ scripts/generate-ssl-certs.sh
@@ -11,7 +11,7 @@ fi
 echo "Generating self-signed certificates..."
 mkdir -p ./config/sslcerts
 openssl genrsa -out ./config/sslcerts/key.pem 4096
-openssl req -new -key ./config/sslcerts/key.pem -out ./config/sslcerts/csr.pem
+openssl req -new -key ./config/sslcerts/key.pem -out ./config/sslcerts/csr.pem -subj "/C=US/ST=The Company State/L=The Compnay City/O=The Company Name/CN=localhost"
 openssl x509 -req -days 365 -in ./config/sslcerts/csr.pem -signkey ./config/sslcerts/key.pem -out ./config/sslcerts/cert.pem
 rm ./config/sslcerts/csr.pem
 chmod 600 ./config/sslcerts/key.pem ./config/sslcerts/cert.pem
