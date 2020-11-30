8d09a9aef220c63161d2de48d8cc3003ddbf1aa8
Julio C. Perez-Baez <perezbjc@gmail.com>
2020-11-30 17:59:03 -0500

    Update header.client.controller.js

--- modules/core/client/controllers/header.client.controller.js
+++ modules/core/client/controllers/header.client.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
-angular.module('core').controller('HeaderController', ['$scope', '$state', 'Authentication', 'Menus',
-  function ($scope, $state, Authentication, Menus) {
+angular.module('core').controller('HeaderController', ['$scope', '$state', '$transitions', 'Authentication', 'Menus',
+  function ($scope, $state, $transitions, Authentication, Menus) {
     // Expose view variables
     $scope.$state = $state;
     $scope.authentication = Authentication;
@@ -10,14 +10,15 @@ angular.module('core').controller('HeaderController', ['$scope', '$state', 'Auth
     $scope.menu = Menus.getMenu('topbar');
 
     // Toggle the menu items
-    $scope.isCollapsed = false;
+    $scope.isCollapsed = true;
     $scope.toggleCollapsibleMenu = function () {
       $scope.isCollapsed = !$scope.isCollapsed;
     };
 
     // Collapsing the menu after navigation
-    $scope.$on('$stateChangeSuccess', function () {
-      $scope.isCollapsed = false;
+    $transitions.onSuccess({}, function(transition) {
+      $scope.isCollapsed = true;
     });
+
   }
 ]);
